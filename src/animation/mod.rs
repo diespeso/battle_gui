033244c020555bbb031274545943f42c049c8cc5
diespeso <edgar.froot@gmail.com>
2020-06-07 16:42:21 -0700

    Animations fully implemented, now is posible to run 2 or more animations sequentially

--- src/animation/mod.rs
+++ src/animation/mod.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::borrow::Borrow;
+use std::ops::Drop;
 
 use super::movable::Movable;
 use ggez::mint::Point2;
@@ -8,136 +9,157 @@ use ggez::timer;
 use core::time::Duration;
 use ggez::Context;
 
-pub static fps: u32 = 60;
+pub static FPS: f64 = 60.0;
 
-pub trait TimedCommand<A>: Command<A> where A: Animatable {
-	fn step(&mut self, t: Rc<RefCell<A>>);
+pub struct Animation {
+	pub animatable: Rc<RefCell<dyn Animatable>>,
+	pub animations: Vec<Box<dyn TimedCommand>>,
+	counter: i32,
 }
 
-pub trait Command<T> {
-	fn execute(&self, t: Rc<RefCell<T>>); //it may be better
-	//to use mut references
+impl Animation {
+	pub fn new(a: Rc<RefCell<dyn Animatable>>) -> Self {
+		Self {
+			animatable: a,
+			animations: Vec::new(),
+			counter: 0,
+		}
+	}
+	
+	pub fn add_animation(&mut self, t_c: Box<dyn TimedCommand>) 
+	{
+		self.animations.push(t_c);
+	}
+	
+	pub fn run(&mut self) {
+		if self.counter < self.animations.len() as i32 {
+			if self.animations[self.counter as  usize].is_completed() {
+			//if this animation is done, get to the next one
+				self.counter += 1;
+			} else {
+				self.animations[self.counter as usize].step(&mut *self.animatable.borrow_mut()); //step current animation
+			}
+		}
+	}
+}
+
+pub trait TimedCommand {
+	fn step(&mut self, a: &mut dyn Animatable);
+	fn get_duration(&self) -> Duration;
+	fn get_completion(&self) -> f64;
+	fn is_completed(&self) -> bool;
+}
+
+pub trait Command {
+	fn execute(&self, a: &mut dyn Animatable);
 }
 
 pub trait Animatable: Movable {
 	
 }
 
-pub struct TimedIdle {
-	duration: Duration,
-	delta: f64,
-	completion: f64,
-}
-
-impl TimedIdle {
-	pub fn new(duration: Duration) -> Self {
-		Self {
-			duration: duration,
-			delta: 1.0 / ((fps as f64) * duration.as_secs_f64()),
-			completion: 0.0,
-		}
-	}	
-}
-
-impl<A> Command<A> for TimedIdle where A: Animatable {
-	fn execute(&self, t: Rc<RefCell<A>>) {
-		//do nothing
-	}
-}
-
-impl<A> TimedCommand<A> for TimedIdle where A: Animatable {
-	fn step(&mut self, t: Rc<RefCell<A>>) {
-		if self.completion >= 1.0  {
-			return;			
-		} else {
-			self.completion += self.delta;
-		}	
-	}
-}
-
 pub struct TimedMove {
-	command: MoveCommand,
+	vector: Point2<f32>,
 	duration: Duration,
-	step_command: MoveCommand,
+	step: Point2<f32>,
 	delta: f64,
 	completion: f64,
 }
 
 impl TimedMove {
-	pub fn new(command: MoveCommand, duration: Duration)
+	pub fn new(vector: Point2<f32>, duration: Duration)
 	-> Self {
-		let (mut x, mut y) = (command.vector.x.clone(), command.vector.y.clone());
-		//println!("{:#?}", duration.as_millis());
-		//x and y how will they change per second?
-		/*x = x / duration.as_millis() as f32;
-		y = y / duration.as_millis() as f32;*/
-		let delta = 1.0 / ((fps as f64) * duration.as_secs_f64());
+		let (mut x, mut y) = (vector.x.clone(),
+			vector.y.clone());
+
+		let delta = 1.0 / (FPS * duration.as_secs_f64());
 		x = x * delta as f32;
 		y = y * delta as f32;		
-		//makes a subcommand that will step its magnitde per second
 		Self {
-			command: command,
+			vector: vector,
 			duration: duration,
-			step_command: MoveCommand::new([x, y].into()),
+			step: Point2::<f32>::from_slice(&[x, y]),
 			delta: delta,
 			completion: 0.0,
 		}
 	}
 }
 
-impl<A> Command<A> for TimedMove where A: Animatable {
-	fn execute(&self, a: Rc<RefCell<A>>) {
-		self.command.execute(a);
+impl Command for TimedMove {
+	fn execute(&self, a: &mut dyn Animatable) {
+		a.move_by(self.vector.clone());
 	}
 }
 
-impl<A> TimedCommand<A> for TimedMove where A: Animatable {
-	fn step(&mut self, a: Rc<RefCell<A>>) {
-		if self.completion >= 1.0  {
-			return;			
+impl TimedCommand for TimedMove {
+	fn step(&mut self, a: &mut dyn Animatable) {
+		if self.is_completed() {
+			return;
 		} else {
-			self.step_command.execute(a);
+			a.move_by(self.step.clone());
 			self.completion += self.delta;
-		}		
-	}
-}
-
-pub struct MoveCommand {
-	vector: Point2<f32>,
-}
-
-impl MoveCommand {
-	pub fn new(vector: Point2<f32>) -> Self {
-		Self{vector}
-	}
-}
-
-impl<T> Command<T> for MoveCommand where T: Animatable {
-	fn execute(&self, t: Rc<RefCell<T>>) {
-		t.borrow_mut().move_by(self.vector);
-	}
-}
-
-pub struct LinearAnimation<T: Animatable> {
-	animatable: Rc<RefCell<T>>,
-	commands: Vec<Box<dyn Command<T>>>
-}
-
-impl<T> LinearAnimation<T> where T: Animatable {
-	pub fn new(animatable: Rc<RefCell<T>>) -> Self {
-		Self{
-			animatable: animatable,
-			commands: Vec::new(),
 		}
+
 	}
 	
-	pub fn add_command(&mut self, animatable: Box<Command<T>>) {
-		self.commands.push(animatable);
+	fn get_duration(&self) -> Duration {
+		self.duration.clone()
 	}
 	
-	pub fn update(&mut self, d: Duration) {
-		for command in &self.commands {
-			command.as_ref().execute(self.animatable.clone());
+	fn get_completion(&self) -> f64 {
+		self.completion.clone()
+	}
+	
+	fn is_completed(&self) -> bool {
+		self.completion >= 1.0
+	}
+}
+
+pub struct TimedIdle {
+	delta: f64,
+	duration: Duration,
+	completion: f64,
+}
+
+impl TimedIdle {
+	pub fn new(duration: Duration) -> Self {
+		Self {
+			delta: 1.0 / (FPS * duration.as_secs_f64()),
+			duration,
+			completion: 0.0,
 		}
 	}
 }
+
+impl Command for TimedIdle {
+	fn execute(&self, a: &mut dyn Animatable) {
+		//do nothing, kinda dumb
+	}
+}
+
+impl TimedCommand for TimedIdle {
+	fn step(&mut self, a: &mut dyn Animatable) {
+		if self.is_completed() {
+			return;
+		} else {
+			self.completion += self.delta;
+		}
+	}
+	
+	fn get_duration(&self) -> Duration {
+		self.duration.clone()
+	}
+	
+	fn get_completion(&self) -> f64 {
+		self.completion.clone()
+	}
+	
+	fn is_completed(&self) -> bool {
+		self.completion >= 1.0
+	}
+}
+
+
+
+
+
